5ddd291f6ac9c5ce63e7c785925793899bb6a3b4
Sansala <136149447+sansalaR@users.noreply.github.com>
2025-03-04 17:53:40 +0530

    links

--- amaran/src/Pages/Home.jsx
+++ amaran/src/Pages/Home.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import Header from '../Components/Layouts/Header'
-import Navbar from '../Components/Layouts/Navbar'
+
 
 const Home = () => {
   return (
     <div>
-     <Header /> 
-     <Navbar />
+    
     </div>
   )
 }
