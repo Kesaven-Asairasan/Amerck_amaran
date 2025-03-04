270b08a54fd3962a72d22056c8ab77b154cecf23
Kesaven Asairasan <kesavenasairasan@Kesavens-MacBook-Air.local>
2025-03-04 17:14:33 +0530

    nav with header

--- amaran/src/Pages/Home.jsx
+++ amaran/src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../Components/Layouts/Header'
+import Navbar from '../Components/Layouts/Navbar'
 
 const Home = () => {
   return (
     <div>
      <Header /> 
+     <Navbar />
     </div>
   )
 }
